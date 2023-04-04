17cc84dff4d6a1ba322223619e3a47d85a857c0a
zacharydserafin <zacharydserafin@gmail.com>
2023-04-04 17:08:35 -0500

    Styled HTML

--- src/template.js
+++ src/template.js
@@ -6,12 +6,12 @@ function createCards(team) {
     for (const member of team) {
         if (member.role === "Manager") {
             const managerHTML = 
-            `\t\t\t<div>
-                <div>
+            `\t\t\t<div class="m-5 zs-card">
+                <div class="p-2 zs-card-header text-start">
                     <h3>${member.name}</h3>
-                    <h4>${member.role}</h4>
+                    <h4>&#128084; ${member.role}</h4>
                 </div>
-                <div>
+                <div class="p-2 zs-card-body">
                     <p>ID: ${member.id}</p>
                     <p>Email: ${member.email}</p>
                     <p>Office Number: ${member.officeNumber}</p>
@@ -21,12 +21,12 @@ function createCards(team) {
         }
         if (member.role === "Engineer") {
             const engineerHTML = 
-            `\t\t\t<div>
-                <div>
+            `\t\t\t<div class="m-5 zs-card">
+                <div class="p-2 zs-card-header text-start">
                     <h3>${member.name}</h3>
-                    <h4>${member.role}</h4>
+                    <h4>&#128083; ${member.role}</h4>
                 </div>
-                <div>
+                <div class="p-2 zs-card-body">
                     <p>ID: ${member.id}</p>
                     <p>Email: ${member.email}</p>
                     <p>GitHub: ${member.github}</p>
@@ -36,12 +36,12 @@ function createCards(team) {
         }
         if (member.role === "Intern") {
             const internHTML = 
-            `\t\t\t<div>
-                <div>
+            `\t\t\t<div class="m-5 zs-card">
+                <div class="p-2 zs-card-header text-start">
                     <h3>${member.name}</h3>
-                    <h4>${member.role}</h4>
+                    <h4>&#127891; ${member.role}</h4>
                 </div>
-                <div>
+                <div class="p-2 zs-card-body">
                     <p>ID: ${member.id}</p>
                     <p>Email: ${member.email}</p>
                     <p>School: ${member.school}</p>
@@ -61,13 +61,16 @@ function compileHTML(cards) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
         <title>My Team Profile</title>
     </head>
     <body>
-        <header>My Team</header>
-        <div>
+        <header class="text-center zs-header">My Team</header>
+        <div class="container text-center d-flex flex-wrap justify-content-center">
 ${cards}
         </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>`;
 return html;
